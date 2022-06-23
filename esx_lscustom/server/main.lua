650c4513a91171050b3325700f0633f4a859f156
Pots <91101609+PotDeCannabis@users.noreply.github.com>
2022-06-23 18:48:21 +0200

    Update main.lua

--- esx_lscustom/server/main.lua
+++ esx_lscustom/server/main.lua
@@ -57,23 +57,3 @@ AddEventHandler('esx_lscustom:refreshOwnedVehicle', function(vehicleProps)
 		end
 	end)
 end)
-
-ESX.RegisterServerCallback('esx_lscustom:getVehiclesPrices', function(source, cb)
-	if not Vehicles then
-		MySQL.Async.fetchAll('SELECT * FROM vehicles', {}, function(result)
-			local vehicles = {}
-
-			for i=1, #result, 1 do
-				table.insert(vehicles, {
-					model = result[i].model,
-					price = result[i].price
-				})
-			end
-
-			Vehicles = vehicles
-			cb(Vehicles)
-		end)
-	else
-		cb(Vehicles)
-	end
-end)
